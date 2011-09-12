2f133c73012ce2383b707ca2e3c8d9a7ab55ea3c
John Holdun <john@johnholdun.com>
2011-09-12 15:09:02 -0400

    dots in the last bit of the nice youtube link screw shit up

--- youtube.com.js
+++ youtube.com.js
@@ -7,7 +7,7 @@ $(function(){
   if (videoIdParts.length) {
     niceUrlParts.push(videoIdParts[1])
     niceUrlParts.push($('a.author').text())
-    niceUrlParts.push(document.title.replace(/[^A-Z0-9 \.]/gi,'').replace(/ +/g,'-').replace(/-youtube$/i, ""))
+    niceUrlParts.push(document.title.replace(/[^A-Z0-9 ]/gi,'').replace(/ +/g,'-').replace(/-youtube$/i, ""))
 
     $("<a>", {
       text: "Descriptive URL",
