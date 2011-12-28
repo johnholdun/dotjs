830f690edf2f4d69b3103abfb18909567bc38c0b
John Holdun <john@johnholdun.com>
2011-12-28 13:05:33 -0800

    fix and IMPROVE copy link

--- youtube.com.js
+++ youtube.com.js
@@ -4,17 +4,20 @@ $(function(){
   var
     videoIdParts = location.href.match(/\?v=(.+?)(&|$)/),
     videoId,
-    niceUrlParts = ['http://youtu.be']
+    niceUrl = "http://youtu.be",
+    videoTitle
 
   if (videoIdParts && videoIdParts.length) {
-    niceUrlParts.push(videoIdParts[1])
-    niceUrlParts.push($('a.author:eq(0)').text())
-    niceUrlParts.push(document.title.replace(/[^A-Z0-9 ]/gi,'').replace(/ +/g,'-').replace(/-youtube$/i, ""))
+    niceUrl += "/" + videoIdParts[1] + "#"
+    niceUrl += $('a.author:eq(0)').text().trim()
+    
+    videoTitle = document.title.replace(/- youtube$/i, "").trim()
+    niceUrl += "/" + videoTitle.replace(/[^A-Z0-9 ]/gi,'').replace(/ +/g,'-')
 
     $("<a>", {
-      text: "Descriptive URL",
+      text: videoTitle,
       title: "Descriptive URL",
-      href: niceUrlParts.join("/"),
+      href: niceUrl,
       style: "display: block; float: right; width: 24px; height: 24px; margin-top: -30px; overflow: hidden; text-indent: -999px; background: url('http://i.imgur.com/wucK0.png') no-repeat center center;"
     })
       .insertBefore($("#watch-description"))
@@ -24,17 +27,26 @@ $(function(){
   
 // Strip it down
 
-if (location.hash.indexOf("full") === -1)
+if (location.pathname === "/" && location.hash.indexOf("full") === -1)
   $(function() {
     $("body").css({
+      background: "#FFF",
       overflowX: "hidden",
       overflowY: "scroll"    
     })
+    
+    $(".ad-div, .guide-container, .guide-background, #feed-background, #video-sidebar, #footer, #logo-container ~ *, .feed-header")
+      .remove()
+      
+    $("#masthead")
+      .css("text-align", "center")
 
-    $("#masthead, #baseDiv").css({
-      width: "640px !important"
-    })
-
-    $("#masthead-utility, #user-navbar, .ad-div, #homepage-side-content, .top-videos-module, #footer-container")
-      .remove()  
+    $("#logo-container")
+      .css({ display: "inline-block", float: "none" })
+      
+    $("#feed")
+      .css({
+        "margin": "0 auto"
+      })
+      
   })
