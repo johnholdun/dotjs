ea56ad3d11631bc3d63b688934ebc11c4cd6e367
John Holdun <john@johnholdun.com>
2011-12-28 13:06:11 -0800

    sometimes you want the ticker

--- facebook.com.js
+++ facebook.com.js
@@ -1,5 +1,6 @@
-$(function() {
-  // KILL YOURSELF
-  $("#pagelet_sidebar, #pagelet_presence, .fbFeedTicker")
-    .remove()
-})
+if (location.hash.indexOf("ticker") === -1)
+  $(function() {
+    // KILL YOURSELF
+    $("#pagelet_sidebar, #pagelet_presence, .fbFeedTicker")
+      .remove()
+  })
