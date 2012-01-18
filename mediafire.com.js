c036605ecd1c9cd05587f9d4923355692850f404
John Holdun <john@johnholdun.com>
2012-01-17 23:47:29 -0500

    did something with the new layout but not much really

--- mediafire.com.js
+++ mediafire.com.js
@@ -1,20 +1,29 @@
 $(function() {
   
-  $("*[id^=remove_ads], .more_dloptions, .footer, .download_options_bar, #header_nav, #logged_in_info").remove()
-  $(".dl_options_wrapper")
-    .css({ width: "500px", margin: "0 auto" })
-    .siblings().remove()
-    
-  $("#v4logo").css({
-    float: "none",
-    width: "200px",
-    margin: "0 auto"
-  })
+  // var $link = $(".download_link").copy()
+  // 
+  // $("#content_container")
+  //   .html()
+  //   .append($link)
+
+  $("div.top, div.dl_options_innerblock .left, div.dl_options_innerblock .right, #dl_options_innerblock ~ *, .social-follow")
+    .remove()
   
-  setTimeout(function() {
-    $(".dl_startlink input[type=checkbox]").removeAttr("checked")    
-  }, 1500)
-    
-  try { QU() } catch(e) { }
+  // $("*[id^=remove_ads], .more_dloptions, .footer, .download_options_bar, #header_nav, #logged_in_info").remove()
+  // $(".dl_options_wrapper")
+  //   .css({ width: "500px", margin: "0 auto" })
+  //   .siblings().remove()
+  //   
+  // $("#v4logo").css({
+  //   float: "none",
+  //   width: "200px",
+  //   margin: "0 auto"
+  // })
+  // 
+  // setTimeout(function() {
+  //   $(".dl_startlink input[type=checkbox]").removeAttr("checked")    
+  // }, 1500)
+  //   
+  // try { QU() } catch(e) { }
   
 })
