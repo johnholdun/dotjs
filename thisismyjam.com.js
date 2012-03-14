9073cba841b91c92b2a2bfd6533c5cbda418ecbf
John Holdun <john@johnholdun.com>
2012-03-14 18:15:17 -0400

    better tweet button yo, remind me to write about this soon

---
+++ thisismyjam.com.js
@@ -0,0 +1,17 @@
+$(function() {
+  var
+    t = $("#jamTitle").text().trim() + " by " + $("#jamArtist").text().trim().replace(/^by ?/, ""),
+    desc = $("blockquote.userDescription").text().trim(),
+    url
+    
+  if (desc.match(/\bthis\b/i)) {
+    t = desc.replace(/\bthis\b/i, t)
+  } else {
+    t += " is my jam. " + desc
+  }
+     
+  url = "https://twitter.com/intent/tweet?text=" + encodeURIComponent(t) + "&url=" + encodeURIComponent(location.href.replace(/(www\.|\/$)/, ""))
+  
+  $("<a>", { "class": "button", "text": "Tweet", "id": "jhJamTweet", "href": url })
+    .insertAfter("#sharePanelCaller")
+})
